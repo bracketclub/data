dbe3a966fe14fe34d0342179a39551e5cbb00eea
Luke Karrys <lukekarrys@gmail.com>
2016-12-08 15:31:25 -0700

    initial removal of api

--- services/api/lib/reply.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const all = (res) => res && res.rows;
-const getRow = (res) => res && res.rows && res.rows.length ? res.rows[0] : null;
-
-module.exports = {all, get: getRow};
--- watchers/lib/logger.js
+++ watchers/lib/logger.js
@@ -3,11 +3,10 @@
 const path = require('path');
 const bucker = require('bucker');
 const config = require('getconfig');
-const _ = require('lodash');
 
 const createLogger = (type) => bucker.createLogger({
   console: {color: true},
-  app: _.extend(config.watchers.logOptions, {
+  app: Object.assign(config.watchers.logOptions, {
     filename: path.resolve(__dirname, '..', '..', 'logs', `${type}.log`)
   })
 });
