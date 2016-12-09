a751b19958816eb97d75ad6fb8f77b538090e03c
Luke Karrys <lukekarrys@gmail.com>
2016-12-08 19:46:19 -0700

    integration

--- integration/entries.js
+++ integration/entries.js
@@ -2,16 +2,14 @@
 
 const Generator = require('bracket-generator');
 
-const createLogger = require('../lib/logger');
-const createSaveEntry = require('../lib/saveEntry');
-const {sport, year, id} = require('../lib/sportYear');
+const {INITIAL, INTERVAL} = require('./interval');
+const createLogger = require('../watchers/lib/logger');
+const createSaveEntry = require('../watchers/lib/saveEntry');
+const {sport, year, id} = require('../watchers/lib/sportYear');
 
 const logger = createLogger(`entries:${id}`);
 const saveEntry = createSaveEntry({logger, sport, year});
 const bracket = new Generator({sport, year});
-
-const INITIAL = 5000;
-const INTERVAL = 5000;
 
 const A = 65;
 const numbers = () => Math.random().toString().slice(2);
---
+++ integration/interval.js
@@ -0,0 +1,8 @@
+/* eslint no-magic-numbers:0 */
+
+'use strict';
+
+module.exports = {
+  INITIAL: 5000,
+  INTERVAL: 60 * 1000
+};
