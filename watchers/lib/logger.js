f0b5985a897c6d66e4babca66eabb9ad40f7c394
Luke Karrys <lukekarrys@gmail.com>
2016-12-10 22:52:50 -0700

    export

--- watchers/lib/logger.js
+++ watchers/lib/logger.js
@@ -4,11 +4,11 @@ const path = require('path');
 const bucker = require('bucker');
 const config = require('getconfig').watchers;
 
-const createLogger = (type) => bucker.createLogger({
+const createLogger = (type, options = {}) => bucker.createLogger({
   console: {color: true},
   app: Object.assign({
     filename: path.resolve(__dirname, '..', '..', 'logs', `${type}.log`)
-  }, config.logOptions)
+  }, config.logOptions, options)
 });
 
 module.exports = createLogger;
