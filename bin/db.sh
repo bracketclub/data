a337bcc2d36807f02a1b84618e1f1060f10f48d0
Luke Karrys <lukekarrys@gmail.com>
2016-12-11 09:35:56 -0700

    integration and db clean script

--- bin/db.sh
+++ bin/db.sh
@@ -1,7 +1,9 @@
-DB_NAME="tweetyourbracket"
-SQL_FILE="tweetyourbracket"
+#!/usr/bin/env bash
 
-dropdb --if-exists $DB_NAME && psql -d postgres -c "DROP ROLE IF EXISTS $DB_NAME"
-psql -d postgres -c "CREATE ROLE $DB_NAME WITH PASSWORD 'password' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN"
+DB_NAME=$1
+
+dropdb --if-exists $DB_NAME
+psql -d postgres -c "DROP ROLE IF EXISTS \"$DB_NAME\""
+psql -d postgres -c "CREATE ROLE \"${DB_NAME}\" WITH PASSWORD 'password' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN"
 createdb $DB_NAME -O $DB_NAME
-psql -d $DB_NAME -f sql/$SQL_FILE.sql
+psql -d $DB_NAME -f sql/$DB_NAME.sql
