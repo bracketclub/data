142bf09499f4c4ae059b227cf865682197f65ef4
Luke Karrys <lukekarrys@gmail.com>
2021-03-13 21:53:08 -0700

    update pm2 script

--- bin/pm2.sh
+++ bin/pm2.sh
@@ -4,4 +4,14 @@ WATCHER=$1
 SPORT=$2
 COMMAND=${3:-start}
 
-./node_modules/.bin/pm2 $COMMAND pm2.config.js --env production --only $WATCHER-$SPORT
+
+if [[ "$COMMAND" == "logs" ]]; then
+  ./node_modules/.bin/pm2 logs $WATCHER-$SPORT
+elif [[ "$NODE_ENV" == "production" || "$(hostname)" == "localhost" ]]; then
+  echo "Starting production mode for $WATCHER $SPORT"
+  ./node_modules/.bin/pm2 $COMMAND pm2.config.js --env production --only $WATCHER-$SPORT
+else
+  echo "Starting dev mode for $WATCHER $SPORT"
+  ./node_modules/.bin/pm2 $COMMAND pm2.config.js --only $WATCHER-$SPORT
+fi
+
