581642571acb23fa69be64018986edfaadd5fcff
Luke Karrys <lukekarrys@gmail.com>
2021-03-13 21:56:18 -0700

    rm newline

--- bin/pm2.sh
+++ bin/pm2.sh
@@ -4,7 +4,6 @@ WATCHER=$1
 SPORT=$2
 COMMAND=${3:-start}
 
-
 if [[ "$COMMAND" == "logs" ]]; then
   ./node_modules/.bin/pm2 logs $WATCHER-$SPORT
 elif [[ "$NODE_ENV" == "production" || "$(hostname)" == "localhost" ]]; then
